d4b756571b394fcef9401d6af57ac05f7914a352
Stefan Beyer <sbeyer@threatint.eu>
2025-01-01 12:20:14 +0100

    working commit, work in progress

---
+++ URL.go
@@ -0,0 +1,36 @@
+package http
+
+import (
+	"net/url"
+	"strings"
+)
+
+func a(urlParameters url.Values, allowedParams []string) (url.Values, bool) {
+	var (
+		v2      url.Values
+		isDirty bool
+	)
+
+	v2 = url.Values{}
+	for key, value := range v {
+		for _, k := range allowedParams {
+			if key == k {
+				v2.Add(k, value[0])
+				if len(value) > 1 {
+					isDirty = true
+				}
+				continue
+			}
+			if strings.ToLower(key) == strings.ToLower(k) {
+				v2.Add(k, value[0])
+				isDirty = true
+				continue
+			}
+		}
+	}
+
+	if isDirty || len(urlParameters) != len(v2) {
+		return v2, true
+	}
+	return urlParameters, false
+}
